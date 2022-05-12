ebfc45f8baee1e6c2a03fcecbb335fbcff3420d2
Martin31110 <hernandezmartin3110@gmail.com>
2022-05-11 20:51:28 -0500

    Agregue un JOptionPane

--- dadasdasd/src/main/java/halley/main.java
+++ dadasdasd/src/main/java/halley/main.java
@@ -1,7 +1,11 @@
 package halley;
 
+import javax.swing.JOptionPane;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        
+        JOptionPane.showMessageDialog(null, "Aaaaa");
     }
 }
